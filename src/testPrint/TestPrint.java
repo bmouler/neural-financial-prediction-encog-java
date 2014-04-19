0bef21f179d1ff6cf5e6c7d89955240cecaab020
bmouler <bmouler@gmail.com>
2014-04-19 14:43:17 -0700

    test: Awesome guide, Kevin. Thanks.

--- src/testPrint/TestPrint.java
+++ src/testPrint/TestPrint.java
@@ -3,7 +3,7 @@ import com.google.gson.Gson;
 
 public class TestPrint {
 	public static void main(String[] args) {
-		System.out.println("Aaron was here!! made a testBranch");
+		System.out.println("Aaron+Bert were here!! made a testBranch");
 		
 		Gson gson = new Gson();
 	}
