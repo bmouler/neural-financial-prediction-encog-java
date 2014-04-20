a552a03ea5748668f925cdd92c895ed9ed2dc0dd
Kevin Rossi <KevinRossi77@gmail.com>
2014-04-20 03:54:06 -0400

    Added functionality for actual and normalized high/low values. This portion is complete. Predict.java needs more work on this.

--- src/KevinThread/__RUNME.java
+++ src/KevinThread/__RUNME.java
@@ -32,20 +32,26 @@ public class __RUNME {
 		String[] DATA_FILES = Props
 				.GetArrayOfStrings(printPropsDebugToConsole, props, "DATA_FILE_");
 		// temporal settings
+		double NORMALIZED_LOW = Props.GetDouble(props, "NORMALIZED_LOW");
+		double NORMALIZED_HIGH = Props.GetDouble(props, "NORMALIZED_HIGH");
 		int INPUT_WINDOW_SIZE = Props.GetInt(props, "INPUT_WINDOW_SIZE");
 		int PREDICT_WINDOW_SIZE = Props.GetInt(props, "PREDICT_WINDOW_SIZE");
 		// training
 		String ACTIVATION_FUNCTION = Props.GetString(props, "ACTIVATION_FUNCTION");
 		double TARGET_ERROR = Props.GetDouble(props, "TARGET_ERROR");
 		// predict
-		int EVALUATE_START = Props.GetInt(props, "EVALUATE_START");
-		int EVALUATE_END = Props.GetInt(props, "EVALUATE_END");
+		// TODO needed?
+		// int EVALUATE_START = Props.GetInt(props, "EVALUATE_START");
+		// int EVALUATE_END = Props.GetInt(props, "EVALUATE_END");
 
 		// get data, then train and evaluate the neural network
 		try {
 			// Step 1. Create training data
 			DataIngester dataIngester = new DataIngester();
-			dataIngester.createData(DEBUG_LEVEL, DATA_FILES);
+			dataIngester.createData(DEBUG_LEVEL, DATA_FILES, NORMALIZED_LOW, NORMALIZED_HIGH);
+			// get actual low/high values for use in prediction output
+			double[] actualLowValues = dataIngester.getActualLowValues();
+			double[] actualHighValues = dataIngester.getActualHighValues();
 
 			TemporalMLDataSet temporalDataset = dataIngester.makeTemporalDataSet(DEBUG_LEVEL,
 					INPUT_WINDOW_SIZE, PREDICT_WINDOW_SIZE);
@@ -58,7 +64,8 @@ public class __RUNME {
 
 			// Step 3. Predict
 			Predict.predict(temporalDataset, model, DEBUG_LEVEL, INPUT_WINDOW_SIZE,
-					PREDICT_WINDOW_SIZE);
+					PREDICT_WINDOW_SIZE, NORMALIZED_LOW, NORMALIZED_HIGH, actualLowValues,
+					actualHighValues);
 
 			// shutdown
 			Encog.getInstance().shutdown();
